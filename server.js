2c21441f298f936965f61a640385b4fe7339435c
linh.tran <tdlinh89@gmail.com>
2022-05-08 00:18:50 +0700

    update

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const FAVORITE_INSTRUCTOR = process.env.NAME;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
+  const favoriteFood = process.env.FAVORITE_FOOD;
+  console.log(`My favorite food is ${favoriteFood}`);
   while(true) {
     console.log("Microservices rock!");
     await sleep(5000);
